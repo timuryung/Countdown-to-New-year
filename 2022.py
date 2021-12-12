577593d9862ded6c25a41be40c655d291f91e59e
timuryunuk2021 <90114599+timuryunuk2021@users.noreply.github.com>
2021-12-12 21:44:49 +0000

    Update 2022.py

--- 2022.py
+++ 2022.py
@@ -1,14 +1,44 @@
+from pywebio.input import *
+from pywebio.output import *
+from pywebio import start_server
+
+from time import sleep
 import datetime
 
-newyear = datetime.date(2022, 1, 1)
-currentdate = datetime.date.today()
-currenttime = datetime.time()
-lefttonewyear = newyear - currentdate
+
+
+def main():
+
+    def newyeardef(): #receiving time to new year
+        newyear = datetime.datetime(datetime.datetime.today().year + 1, 1, 1, 0, 17)
+        
+
+        lefttonewyear = newyear - datetime.datetime.today()
+        if newyear == datetime.datetime.today():
+            newyeartext = 'Happy new' + ' ' + datetime.datetime.today().year + 1 + 'Year! \n'
+            put_text(newyeartext * 6).style('font-size: 60px;')
+            sleep(3153600)
+        else:
+            sleep(1)
+            clear()
+            yearstr = str(datetime.datetime.today().year + 1)
+            put_html('<center> Until the New Year' +  ' ' + yearstr + ', exactly </center>').style('font-size : 60px')
+            put_text(lefttonewyear).style('font-size : 60px; align : center')
+            
+            
 
 
 
-while True:
-    if newyear == currentdate:
-        print('Happy New year!')
-    else:
-        print(lefttonewyear)
+    while True:
+        newyeardef()
+
+start_server(main, port=3588)
+
+
+
+
+
+         
+
+
+
